5b39bf1deeeff0bf06ade305b68dee4e42a256fa
Mengyang Zhang <mengyang.zhang@centific.com>
2024-09-27 17:34:52 +0800

    edit

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -1,6 +1,6 @@
 import { clsx } from 'clsx';
 import Link from 'next/link';
-import { zustand } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 
 interface Breadcrumb {
   label: string;
@@ -15,7 +15,7 @@ export default function Breadcrumbs({
 }) {
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
-      <ol className={clsx(zustand.className, 'flex text-xl md:text-2xl')}>
+      <ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
         {breadcrumbs.map((breadcrumb, index) => (
           <li
             key={breadcrumb.href}
